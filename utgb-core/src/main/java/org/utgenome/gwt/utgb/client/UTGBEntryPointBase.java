f4b295a3025c652e872c991e52ebb4ce976b596d
Taro L. Saito <leo@xerial.org>
2010-05-19 10:39:02 +0900

    remove the GWT log message when scrolling the browser

--- utgb-core/src/main/java/org/utgenome/gwt/utgb/client/UTGBEntryPointBase.java
+++ utgb-core/src/main/java/org/utgenome/gwt/utgb/client/UTGBEntryPointBase.java
@@ -243,7 +243,7 @@ public class UTGBEntryPointBase implements EntryPoint {
 				History.newItem(n, false);
 				String s = propertyReader.getProperty(UTGBProperty.TARGET) + ":" + w.getStartOnGenome() + "-" + w.getEndOnGenome();
 				Window.setTitle(s + " - UTGB");
-				//GWT.log("history: " + n, null);
+				GWT.log("history: " + n, null);
 			}
 
 		}
