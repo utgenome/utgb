ddc6e9328152f6485a22f99d72d8a136f2323183
Taro L. Saito <leo@xerial.org>
2010-07-28 11:10:29 +0900

    add test data

--- utgb-shell/src/test/java/org/utgenome/shell/ImportTest.java
+++ utgb-shell/src/test/java/org/utgenome/shell/ImportTest.java
@@ -66,8 +66,20 @@ public class ImportTest {
 	public void importSAM() throws Exception {
 
 		File tmpSAM = FileUtil.createTempFile(new File("target"), "sample", ".sam");
-		// tmpSAM.deleteOnExit();
+		tmpSAM.deleteOnExit();
 		FileUtil.copy(FileResource.openByteStream(ImportTest.class, "sample.sam"), tmpSAM);
+		// File tmpBAM = FileUtil.createTempFile(new File("target"), "sample", ".bam");
+		UTGBShell.runCommand(new String[] { "import", tmpSAM.getAbsolutePath() });
+
+	}
+
+	@Test
+	public void importPaddedSAMReads() throws Exception {
+
+		File tmpSAM = FileUtil.createTempFile(new File("target"), "test", ".sam");
+		tmpSAM.deleteOnExit();
+
+		FileUtil.copy(FileResource.openByteStream(ImportTest.class, "test.sam"), tmpSAM);
 		// File tmpBAM = FileUtil.createTempFile(new File("target"), "sample", ".bam");
 		UTGBShell.runCommand(new String[] { "import", tmpSAM.getAbsolutePath() });
 
