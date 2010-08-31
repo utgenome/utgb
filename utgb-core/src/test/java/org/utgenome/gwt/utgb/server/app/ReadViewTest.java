0e012767a4833090793fd530f4e89cf4826c71ed
Taro L. Saito <leo@xerial.org>
2010-08-31 17:25:46 +0900

    moved a test code to the proper location

--- utgb-core/src/test/java/org/utgenome/gwt/utgb/server/app/ReadViewTest.java
+++ utgb-core/src/test/java/org/utgenome/gwt/utgb/server/app/ReadViewTest.java
@@ -28,7 +28,6 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 public class ReadViewTest {
-
 	@Test
 	public void testIsDescendantOf() {
 		assertTrue(ReadView.isDescendant("hello.db"));
